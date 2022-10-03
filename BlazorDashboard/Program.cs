471a45b7a28b482b6cdbfed81e51ef29894f40ff
jsakamotoIGJP <jsakamoto@infragistics.com>
2022-10-03 15:43:08 +0900

    MudBlazor の使用を撤去

--- BlazorDashboard/Program.cs
+++ BlazorDashboard/Program.cs
@@ -1,15 +1,12 @@
 using BlazorDashboard;
+using BlazorDashboard.Services;
+using IgniteUI.Blazor.Controls;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using IgniteUI.Blazor.Controls;
-using BlazorDashboard.Services;
-using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(typeof(IIgniteUIBlazor), typeof(IgniteUIBlazor));
