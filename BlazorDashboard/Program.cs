5ec7d1aa23fd85a11e7bf3c46c04dd2691f0b00c
jsakamotoIGJP <jsakamoto@infragistics.com>
2022-10-03 16:24:33 +0900

    IGnite UI for Blazor はじめ、参照パッケージのバージョンを更新

--- BlazorDashboard/Program.cs
+++ BlazorDashboard/Program.cs
@@ -1,6 +1,5 @@
 using BlazorDashboard;
 using BlazorDashboard.Services;
-using IgniteUI.Blazor.Controls;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,8 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(typeof(IIgniteUIBlazor), typeof(IgniteUIBlazor));
+builder.Services.AddIgniteUIBlazor();
 builder.Services.AddSingleton<DashboardService>();
-
 
 await builder.Build().RunAsync();
