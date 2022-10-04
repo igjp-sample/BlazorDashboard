a4b459fc995014786a81c7f5395f70a193cf48e8
jsakamotoIGJP <jsakamoto@infragistics.com>
2022-10-04 15:02:17 +0900

    ChartView での Json.NET の使用を削除

---
+++ BlazorDashboard/Components/ChartView.razor.js
@@ -0,0 +1,11 @@
+﻿export const setDataSource = (containerElement, dataSourceJson) => {
+
+  const dataChart = containerElement.querySelector('igc-data-chart');
+  const series = dataChart.actualSeries;
+  const axis = dataChart.actualAxes;
+
+  const dataSourceObject = JSON.parse(dataSourceJson);
+  dataChart.dataSource = dataSourceObject;
+  series[0].dataSource = dataSourceObject;
+  axis[0].dataSource = dataSourceObject;
+};
