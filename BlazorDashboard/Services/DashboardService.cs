f79d8072e87fae3f91c2eddca8973fe77481945a
jsakamotoIGJP <jsakamoto@infragistics.com>
2022-10-04 15:16:14 +0900

    null許容参照型に関する警告に対処

--- BlazorDashboard/Services/DashboardService.cs
+++ BlazorDashboard/Services/DashboardService.cs
@@ -2,176 +2,112 @@
 using System.Data;
 using Infragistics.Documents.Excel;
 
-namespace BlazorDashboard.Services
+namespace BlazorDashboard.Services;
+
+public record DashboardItem
+(
+    string ID,
+    string Title,
+    DataTable DataTable
+);
+
+public class DashboardService
 {
-    public class DashboardService
+    public ObservableCollection<DashboardItem> Items { get; } = new();
+
+    public void AddDashboardItem(string fileName, byte[] fileContent)
     {
+        using var ms = new MemoryStream(fileContent);
+        var workBook = Workbook.Load(ms);
+        var dataTable = ConvertWorksheetToDataTable(workBook.Worksheets[0]);
+        var item = new DashboardItem(Guid.NewGuid().ToString(), fileName, dataTable);
+        this.Items.Add(item);
+    }
 
-        public ObservableCollection<DashboardItem> Items { get; set; }
+    private static DataTable ConvertWorksheetToDataTable(Worksheet worksheet)
+    {
+        // Tableを作成するための情報を収集する。
+        // 追加用のDataTable
+        var newDataTable = new DataTable(worksheet.Name);
 
-        public delegate void AddDashboardItemMethod(DashboardItem item);
-
-        public DashboardService()
+        foreach (var headerCell in worksheet.Rows[0].Cells)
         {
-            this.Items = new ObservableCollection<DashboardItem>();
-        }
+            var dataCell = worksheet.Rows[1].Cells[headerCell.ColumnIndex];
 
-        public void AddDashboardItem(byte[] content)
-        {
-            var item = new DashboardItem();
-            item.ID = "123";
-            item.Title = "aiueo";
+            // カラムを追加
+            var headerColumn = new DataColumn();
+            headerColumn.ColumnName = headerCell.GetText();
 
-            //var ms = new MemoryStream(content);
-            //var wb = Workbook.Load(ms);
-            //item.Workbook = wb;
+            // 2行目を型の判定に利用する。
+            headerColumn.DataType = typeof(string);
 
-            //item.DataTable = ConvertDTbyWB(wb);
+            var cellText = dataCell.GetText(TextFormatMode.IgnoreCellWidth);
 
-
-            this.Items.Add(item);
-        }
-
-
-        public void AddDashboardItem(string fileName, byte[] fileContent)
-        {
-            var item = new DashboardItem();
-            item.Title = fileName;
-
-            var ms = new MemoryStream(fileContent);
-            var wb = Workbook.Load(ms);
-
-            item.Workbook = wb;
-            item.DataTable = this.ConvertWSToDT(wb.Worksheets[0]);
-
-            this.Items.Add(item);
-        }
-
-
-        public void AddDashboardItem(string fileName, Workbook wb)
-        {
-            var item = new DashboardItem();
-            item.ID = "123";
-            item.Title = fileName;
-
-            item.Workbook = wb;
-            item.DataTable = this.ConvertWSToDT(wb.Worksheets[0]);
-
-
-            //Items.Add(item);
-        }
-
-        public DataTable ConvertWSToDT(Worksheet worksheet)
-        {
-            // Tableを作成するための情報を収集する。
-            // 追加用のDataTable
-            var newDataTable = new DataTable(worksheet.Name);
-
-            foreach (var headerCell in worksheet.Rows[0].Cells)
+            if (string.IsNullOrEmpty(cellText))
             {
-
-                var dataCell = worksheet.Rows[1].Cells[headerCell.ColumnIndex];
-
-                // カラムを追加
-                var headerColumn = new DataColumn();
-                headerColumn.ColumnName = headerCell.GetText();
-
-                // 2行目を型の判定に利用する。
-
                 headerColumn.DataType = typeof(string);
-
-
-                var cellText = dataCell.GetText(TextFormatMode.IgnoreCellWidth);
-
-
-                if (string.IsNullOrEmpty(cellText))
+            }
+            else
+            {
+                // 日付にコンバートできるか
+                if (DateTime.TryParse(cellText, out var _))
                 {
-                    headerColumn.DataType = typeof(string);
+                    headerColumn.DataType = typeof(DateTime);
                 }
-                else
+                else if (Decimal.TryParse(cellText, out var _))
                 {
-                    // 日付にコンバートできるか
-                    DateTime tryParseDate;
-                    Decimal tryParseDecimal;
-                    if (DateTime.TryParse(cellText, out tryParseDate))
-                    {
-                        headerColumn.DataType = typeof(DateTime);
-                    }
-                    else if (Decimal.TryParse(cellText, out tryParseDecimal))
-                    {
-                        headerColumn.DataType = typeof(decimal);
-                    }
+                    headerColumn.DataType = typeof(decimal);
                 }
-
-
-                newDataTable.Columns.Add(headerColumn);
             }
 
-            // データを挿入します。
-            var currentRowIdx = 0;
-            foreach (var row in worksheet.Rows)
+            newDataTable.Columns.Add(headerColumn);
+        }
+
+        // データを挿入します。
+        foreach (var row in worksheet.Rows.Skip(1))
+        {
+            // 1レコード分のデータを取得
+            var addRow = newDataTable.NewRow();
+            for (var c = 0; c < newDataTable.Columns.Count; c++)
             {
-                currentRowIdx++;
-                // 1行目はスルー
-                if (currentRowIdx <= 1) continue;
+                var cellText = row.Cells[c].GetText(TextFormatMode.IgnoreCellWidth);
 
-                // 1レコード分のデータを取得
-                var addRow = newDataTable.NewRow();
-                for (var c = 0; c < newDataTable.Columns.Count; c++)
+                // 日付にコンバートできるか
+                if (DateTime.TryParse(cellText, out var tryParseDate))
                 {
-                    var cellText = row.Cells[c].GetText(TextFormatMode.IgnoreCellWidth);
-
-                    // 日付にコンバートできるか
-                    DateTime tryParseDate;
-                    Decimal tryParseDecimal;
-                    if (DateTime.TryParse(cellText, out tryParseDate))
+                    if (tryParseDate.Year >= 1900)
                     {
-                        if (tryParseDate.Year >= 1900)
-                        {
-                            addRow[newDataTable.Columns[c]] = tryParseDate;
-                        }
-                        else if (Decimal.TryParse(cellText, out tryParseDecimal))
-                        {
-                            addRow[newDataTable.Columns[c]] = tryParseDecimal;
-                        }
-                        else
-                        {
-                            addRow[newDataTable.Columns[c]] = row.Cells[c].Value;
-                        }
-
+                        addRow[newDataTable.Columns[c]] = tryParseDate;
                     }
-                    else if (Decimal.TryParse(cellText, out tryParseDecimal))
+                    else if (Decimal.TryParse(cellText, out var tryParseDecimal))
                     {
                         addRow[newDataTable.Columns[c]] = tryParseDecimal;
                     }
                     else
                     {
-                        if (row.Cells[c].Value == null)
-                        {
-                            addRow[newDataTable.Columns[c]] = DBNull.Value;
-                        }
-                        else
-                        {
-                            addRow[newDataTable.Columns[c]] = row.Cells[c].Value;
-                        }
-
+                        addRow[newDataTable.Columns[c]] = row.Cells[c].Value;
                     }
-
-
                 }
-                newDataTable.Rows.Add(addRow);
+                else if (Decimal.TryParse(cellText, out var tryParseDecimal))
+                {
+                    addRow[newDataTable.Columns[c]] = tryParseDecimal;
+                }
+                else
+                {
+                    if (row.Cells[c].Value == null)
+                    {
+                        addRow[newDataTable.Columns[c]] = DBNull.Value;
+                    }
+                    else
+                    {
+                        addRow[newDataTable.Columns[c]] = row.Cells[c].Value;
+                    }
+                }
             }
-
-            return newDataTable;
+            newDataTable.Rows.Add(addRow);
         }
 
-    }
-
-    public class DashboardItem
-    {
-        public string ID { get; set; }
-        public string Title { get; set; }
-        public Workbook Workbook { get; set; }
-        public DataTable DataTable { get; set; }
+        return newDataTable;
     }
 }
+
