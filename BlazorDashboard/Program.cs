1fad1d3e59678f607e222279feb2a6e1be8e62dc
jsakamotoIGJP <jsakamoto@infragistics.com>
2026-03-06 17:28:14 +0900

    Blazored.LocalStorage への依存を削除

--- BlazorDashboard/Program.cs
+++ BlazorDashboard/Program.cs
@@ -1,6 +1,5 @@
 using BlazorDashboard;
 using BlazorDashboard.Services;
-using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,7 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddIgniteUIBlazor();
 builder.Services.AddSingleton<DashboardService>();
-builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<DockManagerHelper>();
 
 await builder.Build().RunAsync();
