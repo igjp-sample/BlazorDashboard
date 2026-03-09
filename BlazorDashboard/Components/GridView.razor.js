1500988b7f31dbdd49cdc729e8427fa744dd1c0e
jsakamotoIGJP <jsakamoto@infragistics.com>
2026-03-09 11:56:44 +0900

    IgbDataGrid から IgbGrid に移行

--- BlazorDashboard/Components/GridView.razor.js
+++ BlazorDashboard/Components/GridView.razor.js
@@ -1,26 +1,18 @@
-﻿const getDataGrid = (containerElement) => {
-  return new Promise((resolve, reject) => {
-    const dataGrid = containerElement.querySelector('igc-data-grid');
-    if (dataGrid !== null) { resolve(dataGrid); }
-    else {
-      let counter = 0;
-      const timerId = setInterval(() => {
-        counter++;
-        const dataGrid = containerElement.querySelector('igc-data-grid');
-        if (dataGrid !== null) {
-          clearInterval(timerId);
-          resolve(dataGrid);
-        }
-        else if (counter > (5000 / 10)) {
-          clearInterval(timerId);
-          reject();
-        }
-      }, 10)
-    }
-  });
-};
+const getGrid = async (containerElement) => {
+  for (let i = 0; i < 50; i++) {
+    const grid = containerElement.querySelector('igc-grid');
+    if (grid) return grid;
+    await new Promise(resolve => setTimeout(resolve, 100));
+  }
+  throw new Error('Grid not found within the specified time.');
+}
 
 export const setDataSource = async (containerElement, dataSourceObject) => {
-  const dataGrid = await getDataGrid(containerElement);
-  dataGrid.dataSource = dataSourceObject;
+  const grid = await getGrid(containerElement);
+  grid.data = dataSourceObject;
+  await new Promise(resolve => setTimeout(resolve, 1)); // Allow time for the grid to update
+  grid.columns.forEach(column => {
+    column.sortable = true;
+    column.resizable = true;
+  });
 };
