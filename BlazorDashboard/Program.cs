2d1cfb7b834c439107b6556df52762258455ebe3
jsakamotoIGJP <jsakamoto@infragistics.com>
2026-03-09 16:28:45 +0900

    英語と日本語のローカリゼーションを実装

--- BlazorDashboard/Program.cs
+++ BlazorDashboard/Program.cs
@@ -2,12 +2,14 @@ using BlazorDashboard;
 using BlazorDashboard.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddI18nText();
 builder.Services.AddIgniteUIBlazor();
 builder.Services.AddSingleton<DashboardService>();
 builder.Services.AddScoped<DockManagerHelper>();
